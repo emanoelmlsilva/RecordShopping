c05829621108ece13cabd7898b099d13959f4ca7
emanoelmedeiros <emanoelmlsilva83@gmail.com>
2020-09-21 23:04:58 -0300

    corrigir nome de cores secundarias

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -3,11 +3,11 @@ const colors = {
   colorPrimaryLight: "#00FF84",
   colorPrimaryLight: "#00994F",
   colorSecondary: "#DAE5DF",
-  colorPrimaryLight: "#F2FFF8",
-  colorPrimaryLight: "#AAB3AE",
+  colorSecondaryLight: "#F2FFF8",
+  colorSecondaryLight: "#AAB3AE",
   colorAccent: "#00F200",
   colorTextPrimary: "#212121",
-  colorTextSecondary: "#F1F1F1",
+  colorTextSecondary: "#FFFFFF",
 };
 
 export default colors;
