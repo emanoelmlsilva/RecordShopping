7e3daef13a366137efde6bfd435a905b34886a69
emanoelmedeiros <emanoelmlsilva83@gmail.com>
2020-10-15 22:26:41 -0300

    adicionar campo year em objeto

--- src/util/Convert.js
+++ src/util/Convert.js
@@ -6,18 +6,24 @@ function convertDataToObjectDetail(data) {
     const objectShoppingStore = {
       date: "",
       month: "",
+      year: "",
       store: "",
       ShoppingStores: [],
     };
     if (choiceDate.indexOf(item.date) === -1 && item.date !== "") {
       const separateData = item.date.split(" ");
-      [objectShoppingStore.date, objectShoppingStore.month] = separateData;
+      [
+        objectShoppingStore.date,
+        objectShoppingStore.month,
+        objectShoppingStore.year,
+      ] = separateData;
       objectShoppingStore.store = item.store;
       data.shopping.map((item2) => {
         const objectShopping = {
           product: "",
           category: "",
           amount: 0,
+          parcel: 0,
           price: 0.0,
         };
         if (item.date === item2.date) {
@@ -25,6 +31,7 @@ function convertDataToObjectDetail(data) {
           objectShopping.category = item.category;
           objectShopping.amount = item.payment.amount;
           objectShopping.price = item.payment.price;
+          objectShopping.parcel = item.payment.parcel;
           objectShoppingStore.ShoppingStores.push(objectShopping);
         }
         return true;
