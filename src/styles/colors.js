09510452ced1aa6f9e35d702846a3e8f2df66137
emanoelmedeiros <emanoelmlsilva83@gmail.com>
2020-10-19 17:07:46 -0300

    adicionar cores dos cartões

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -8,6 +8,12 @@ const colors = {
   colorAccent: "#00F200",
   colorTextPrimary: "#212121",
   colorTextSecondary: "#FFFFFF",
+  colorCardDefault: ["#C4C4BE", "#000"],
+  colorCard01: ["#ce03ff", "#00FF84"],
+  colorCard02: ["#FDE500", "#ff00f7"],
+  colorCard03: ["#6C00FF", "#00F3FF"],
+  colorCard04: ["#00F9FF", "#09FF00"],
+  colorCard05: ["#FF008D", "#FF6A00"],
+  colorCard06: ["#FF00AA", "#1DFF00"],
 };
-
 export default colors;
