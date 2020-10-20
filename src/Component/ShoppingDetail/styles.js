fdc1288d6b5fb68e6d21a5360c861f08dfb389a3
emanoelmedeiros <emanoelmlsilva83@gmail.com>
2020-10-20 17:08:09 -0300

    criar estilo

---
+++ src/Component/ShoppingDetail/styles.js
@@ -0,0 +1,64 @@
+import { StyleSheet } from "react-native";
+import { colors, fonts, metrics } from "../../styles";
+
+const HEIGHT_PERCENTAGE = 0.6;
+const WIDTH_PERCENTAGE = 0.9;
+const styles = StyleSheet.create({
+  frame: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+    backgroundColor: "rgba(0, 0, 0, 0.6)",
+  },
+  container: {
+    height: metrics.screenHeight * HEIGHT_PERCENTAGE,
+    width: metrics.screenWidth * WIDTH_PERCENTAGE,
+  },
+  containerHead: {
+    flex: 1,
+    borderTopLeftRadius: metrics.baseBorder,
+    borderTopRightRadius: metrics.baseBorder,
+    backgroundColor: colors.colorPrimary,
+    elevation: 2,
+  },
+  containerTitle: {
+    flexDirection: "row",
+    margin: metrics.baseMargin,
+    marginBottom: 0,
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  containerInfo: {
+    flex: 4,
+    alignItems: "center",
+    backgroundColor: colors.colorSecondaryLight,
+  },
+  contianerHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: metrics.screenWidth * 0.9,
+    padding: metrics.baseMargin,
+  },
+  divider: {
+    margin: metrics.smallMargin,
+    backgroundColor: "#000",
+    height: 1,
+    width: metrics.screenWidth * 0.9,
+  },
+  titleHeader: {
+    fontSize: fonts.regular,
+    fontWeight: "bold",
+  },
+  title: {
+    marginLeft: metrics.baseMargin,
+    fontSize: fonts.title,
+    fontWeight: "bold",
+    flex: 1,
+  },
+  date: {
+    paddingLeft: 40,
+    fontSize: fonts.small,
+  },
+});
+
+export default styles;
