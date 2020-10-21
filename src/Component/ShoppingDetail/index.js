8631b9837996f295c0a0aa05165fdcba98018a55
emanoelmedeiros <emanoelmlsilva83@gmail.com>
2020-10-20 21:15:49 -0300

    adicionar flatlist para listar compara realizadas

--- src/Component/ShoppingDetail/index.js
+++ src/Component/ShoppingDetail/index.js
@@ -1,22 +1,25 @@
 import React from "react";
-import { View, Text, Modal } from "react-native";
-import styles from "./styles";
+import { View, Text, Modal, FlatList } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
-import { colors } from "../../styles";
 import { Divider } from "react-native-paper";
+import PropType from "prop-types";
+import styles from "./styles";
+import { colors } from "../../styles";
 
 const ShoppingDetail = (props) => {
-  const {
-    name,
-    nickName,
-    date,
-    icon,
-    isVisible,
-    onCancel,
-    products,
-    priceTotal,
-    typeShopping,
-  } = props;
+  const { name, date, icon, isVisible, onCancel, products, priceTotal } = props;
+
+  const renderItem = ({ item }) => {
+    return (
+      <View style={styles.containerDetail}>
+        <Text style={styles.columnProducts}>{item.name}</Text>
+        <Text style={styles.columnProducts}>
+          {`${item.category} - qt${item.amount}`}
+        </Text>
+        <Text style={styles.columnProducts}>{`R$ ${item.price}`}</Text>
+      </View>
+    );
+  };
 
   return (
     <Modal
@@ -47,11 +50,35 @@ const ShoppingDetail = (props) => {
               <Text style={styles.titleHeader}>Preço</Text>
             </View>
             <Divider style={styles.divider} />
+            <FlatList
+              data={products}
+              showsVerticalScrollIndicator={false}
+              renderItem={renderItem}
+              keyExtractor={(item, index) => String(index)}
+            />
+            <Divider style={styles.divider} />
+            <View style={styles.contianerFooter}>
+              <Text style={styles.titleHeader}>Total</Text>
+              <Text>
+                R$
+                {priceTotal}
+              </Text>
+            </View>
           </View>
         </View>
       </View>
     </Modal>
   );
+};
+
+ShoppingDetail.propTypes = {
+  name: PropType.string.isRequired,
+  date: PropType.string.isRequired,
+  icon: PropType.string.isRequired,
+  isVisible: PropType.bool.isRequired,
+  onCancel: PropType.func.isRequired,
+  products: PropType.arrayOf(PropType.object).isRequired,
+  priceTotal: PropType.string.isRequired,
 };
 
 export default ShoppingDetail;
